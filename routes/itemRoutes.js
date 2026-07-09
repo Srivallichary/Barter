3b6c7760b6a0244a2f5797f1c0b4d5bd8098d0f6
Srivallichary <kammarisrivallichary@gmail.com>
2026-07-09 16:25:04 +0530

    Complete Member 1 backend controllers and routes

---
+++ routes/itemRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    createItem,
+    getAllItems,
+    getItemById,
+    updateItem,
+    deleteItem,
+    getSmartMatches
+} = require("../controllers/itemController");
+
+// Create Item
+router.post("/", createItem);
+
+// Get All Items
+router.get("/", getAllItems);
+
+// Get Smart Matches
+router.get("/matches/:id", getSmartMatches);
+
+// Get Single Item
+router.get("/:id", getItemById);
+
+// Update Item
+router.put("/:id", updateItem);
+
+// Delete Item
+router.delete("/:id", deleteItem);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const itemRoutes = require("./routes/itemRoutes");
+const tradeRoutes = require("./routes/tradeRoutes");
 
 dotenv.config();
-// connectDB();
+connectDB();
 
 const app = express();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
+app.use("/api/trades", tradeRoutes);
+
 // Test Route
 app.get("/", (req, res) => {
-    res.send("Barter API is running 🚀");
+    res.send("Barter API is running ");
 });
 
 const PORT = process.env.PORT || 5000;
